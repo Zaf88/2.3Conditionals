7fbc3c666446a09eddb925d15f2bbefb264eea91
Your Name <applezafar@gmail.com>
2022-06-23 23:07:11 +1000

    homework additional

--- src/Main.java
+++ src/Main.java
@@ -3,18 +3,15 @@ public class Main {
     public static void main(String[] args) {
 
         int age = 19;
-        int salary = 59_000;
+        int salary = 85_000;
+        int creditLimit = 0;
         if (age >= 23) {
-            System.out.println("We are ready to issue you a credit card with a limit" + 3 * salary + "roubles");
-        }  if (salary >= 80_000) {
+            System.out.println("creditLimit" + 3 * salary + "roubles");
+        } else
+            System.out.println("creditLimit" + 2 * salary + "roubles");
+
+        if (salary >= 80_000) {
             System.out.println("creditLimit + increased + 1.5 times");
-        }
-        if (age < 23) {
-            System.out.println("We are ready to issue you a credit card with a limit" + 2 * salary + "roubles");
-        }
-         else if (salary >= 50_000) {
-            System.out.println("creditLimit + increased + 1.2 times");
-        }
-    }
-}
+        } else
+            System.out.println("creditLimit + increased + 1.2 times");}}
 
